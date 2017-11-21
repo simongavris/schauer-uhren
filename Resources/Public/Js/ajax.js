8a4e50a60f47518fe64450b6c57a06216deefa4c
rawk7000 <ce@rawk.at>
2017-11-21 16:17:08 +0100

    Update ajax.js
    added next/previous logic, number to page

--- Resources/Public/Js/ajax.js
+++ Resources/Public/Js/ajax.js
@@ -1,16 +1,19 @@
 $( document ).ready(function() {
 
 	
-	function loadElements (element, productID, xID, cat01, cat02, number, limit){
+	function loadElements (element, productID, xID, cat01, cat02, page, limit){
 		
 		/*var ajaxUrl = 'index.php?id=192&L=0&no_cache=1&tx_rmmattigschauer_msproducts%5Baction%5D=ajax&tx_rmmattigschauer_msproducts%5Bcontroller%5D=Produkte';*/
 		var ajaxUrl = element.attr('href');
 		var container = element.closest('.generated-products').find('#generated-results');
 		var limit = $('#ms-limit').val();
-		var number = $('#ms-page').val();
+		
+		if(!page)
+			var page = $('#ms-page').val();
+		
 		var cat00 = $('#ms-getcat').val();
 		
-		console.log(ajaxUrl + " - " + productID + " - " + xID + " - " + cat01 + " - " + cat02 + " - " + number + " - " + limit);
+		console.log(ajaxUrl + " - " + productID + " - " + xID + " - " + cat01 + " - " + cat02 + " - " + page + " - " + limit);
 		
 		if (ajaxUrl !== undefined && ajaxUrl !== '') {
 			
@@ -25,15 +28,17 @@ $( document ).ready(function() {
 					"tx_rmmattigschauer_msproducts[cat00]": parseInt(cat00),
 					"tx_rmmattigschauer_msproducts[cat01]": parseInt(cat01),
 					"tx_rmmattigschauer_msproducts[cat02]": parseInt(cat02),
-					"tx_rmmattigschauer_msproducts[number]": parseInt(number),
+					"tx_rmmattigschauer_msproducts[number]": parseInt(page),
 					"tx_rmmattigschauer_msproducts[limit]": parseInt(limit)
 				},
 				success: function (result) {
 					
-					console.log("success");
+					//console.log("success");
 					var ajaxDom = $(result).find('.tx-rm-mattigschauer').children();
 					
-					console.log(ajaxDom);
+					$('#ms-page').val(page);
+					
+					//console.log(ajaxDom);
 					
 					//$('#' + container).replaceWith(ajaxDom);
 					//$('#'+container+' .page-navigation').remove();
@@ -66,6 +71,7 @@ $( document ).ready(function() {
 		var selBox01 = $('#analog_kat').find(":selected").val();
 		var selBox02 = $('#analog_uw').find(":selected").val();
 		var element = $(this).closest('.generated-products').find('.load_button');//.find('.load_more');
+		$('#ms-page').val(0);
 
 		loadElements(element, '156', '12', selBox01, selBox02);
 	
@@ -76,8 +82,49 @@ $( document ).ready(function() {
 		var selBox01 = $('#analog_kat').find(":selected").val();
 		var selBox02 = $('#analog_uw').find(":selected").val();
 		var element = $(this).closest('.generated-products').find('.load_button');
+		$('#ms-page').val(0);
 
 		loadElements(element, '156', '12', selBox01, selBox02);
+		
+	});
+	
+		/* next button */
+	
+	$('.generated-products').on('click', '.ms_next', function(e) {
+		
+		//console.log("next");
+		
+		var selBox01 = $('#analog_kat').find(":selected").val();
+		var selBox02 = $('#analog_uw').find(":selected").val();
+		var element = $(this).closest('.generated-products').find('.load_button');
+		var limit = $('#ms-limit').val();
+		var page = parseInt($('#ms-page').val())+1;
+
+		
+		loadElements(element, '156', '12', selBox01, selBox02, page, limit);
+		
+	});
+	
+	$('.generated-products').on('click', '.ms_previous', function(e) {
+		
+		//console.log("previous");
+		
+		var selBox01 = $('#analog_kat').find(":selected").val();
+		var selBox02 = $('#analog_uw').find(":selected").val();
+		var element = $(this).closest('.generated-products').find('.load_button');
+		var limit = $('#ms-limit').val();
+		var page = 0;
+		
+		if(parseInt($('#ms-page').val()) > 0){
+			console.log("geht no:"+parseInt($('#ms-page').val()));
+			page = parseInt($('#ms-page').val())-1;
+			
+			loadElements(element, '156', '12', selBox01, selBox02, page, limit);
+			
+		}else{
+			//console.log("geht nimma");
+			//page = parseInt($('#ms-page').val());
+		}
 		
 	});
 	
